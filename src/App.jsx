922c93d6eaba7e394f6077b150a91cc1c6656d14
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2023-12-27 16:00:07 -0300

    FEAT: Adicionada a rota about-us com a page respectiva

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import TemplateRoot from "./pages/TemplateRoot";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import "./App.css";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <TemplateRoot />,
-      children: [{ index: true, element: <Home /> }],
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about-us", element: <AboutUs /> },
+      ],
     },
   ]);
 
