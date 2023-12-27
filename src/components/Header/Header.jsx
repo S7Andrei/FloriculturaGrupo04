b2bee9f777b42fcfb93a282ed2c5f85eaade8bba
Pedro Souza <pedrosport10@gmail.com>
2023-12-27 15:39:40 -0300

    REFACTOR: Limpando estilos padrão

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <header>
+        {/*Logo a ser inserida: <img src={} />*/}
+        <ul>
+            <li>
+                <button>Home</button>
+            </li>
+            <li>
+                <button>Register</button>
+            </li>
+            <li>
+                <button>Products</button>
+            </li>
+            <li>
+                <button>About us</button>
+            </li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
