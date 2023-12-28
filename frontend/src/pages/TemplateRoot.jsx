83933ffd40b6553ed380e9fe62614d306bc219c1
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2023-12-27 23:12:23 -0300

    FEAT: Adicionado os components Header e Footer para o TemplateRoot

--- frontend/src/pages/TemplateRoot.jsx
+++ frontend/src/pages/TemplateRoot.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
 
 function TemplateRoot() {
   return (
     <>
-      <h1>Template</h1>
+      <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
