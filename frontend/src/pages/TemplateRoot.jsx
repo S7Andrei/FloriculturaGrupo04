00974b2a1f370e54d44986947bb1723d2674ae19
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2023-12-28 17:48:22 -0300

    FIX: Lógica do get refatorada para não dar conflitos

--- frontend/src/pages/TemplateRoot.jsx
+++ frontend/src/pages/TemplateRoot.jsx
@@ -3,11 +3,15 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import { useDispatch } from "react-redux";
 import { plantsActions } from "../store/plants/plantsSlice";
+import { useEffect } from "react";
 
 function TemplateRoot() {
   const data = useLoaderData();
   const dispatch = useDispatch();
-  dispatch(plantsActions.handleGetPlants(data));
+
+  useEffect(() => {
+    dispatch(plantsActions.handleGetPlants(data));
+  }, [dispatch, data]);
 
   return (
     <>
