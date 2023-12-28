111c5aad3dc5d7afc28f0dc1821870a5d9207698
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2023-12-28 00:12:38 -0300

    REFACTOR: Header refatorado com os links da páginas

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,23 +1,29 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
-        {/*Logo a ser inserida: <img src={} />*/}
+      {/*Logo a ser inserida: <img src={} />*/}
+      <nav>
         <ul>
-            <li>
-                <button>Home</button>
-            </li>
-            <li>
-                <button>Register</button>
-            </li>
-            <li>
-                <button>Products</button>
-            </li>
-            <li>
-                <button>About us</button>
-            </li>
+          <li>
+            <NavLink to={"/"} end>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/register"}>Register</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/products"}>Products</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/about-us"}>About us</NavLink>
+          </li>
         </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
