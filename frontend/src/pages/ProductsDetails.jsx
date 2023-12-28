667db59611f8d656b3eb5ee946f5b8c1ce53caef
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2023-12-28 15:31:20 -0300

    FEAT: Protótipo para a rota de detalhes do produto individual finalizada

--- frontend/src/pages/ProductsDetails.jsx
+++ frontend/src/pages/ProductsDetails.jsx
@@ -1,9 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { plantsActions } from "../store/plants/plantsSlice";
+import { useEffect } from "react";
 
 const ProductsDetails = () => {
-  const params = useParams();
+  const plantSelected = useSelector((state) => state.plants.plantSelected);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const plantId = parseInt(id);
 
-  return <h1>ProductsDetails, id: {params.id}</h1>;
+  useEffect(() => {
+    dispatch(plantsActions.handleGetPlantDetails(plantId));
+  }, [plantId, dispatch]);
+
+  console.log(plantSelected);
+
+  return (
+    <>
+      <h1>{plantSelected.plantName}</h1>
+      <h2>{plantSelected.id}</h2>
+    </>
+  );
 };
 
 export default ProductsDetails;
