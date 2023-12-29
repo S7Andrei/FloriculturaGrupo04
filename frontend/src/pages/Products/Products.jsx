d974269ce46448ae3020f39f788304147d138d15
Pedro Souza <pedrosport10@gmail.com>
2023-12-29 15:46:36 -0300

    REFACTOR: Ajustando products e seu css

--- frontend/src/pages/Products/Products.jsx
+++ frontend/src/pages/Products/Products.jsx
@@ -1,26 +1,33 @@
 /* eslint-disable react/prop-types */
-import CardContent from "../../components/CardContent/CardContent";
-import './styles.css'
+import CardContent from "../../components/CardContent/CardContent.jsx";
 import { useSelector } from "react-redux";
+import styles from "./styles.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Products = ({ listagemTotal = false, filter }) => {
-
   const plants = useSelector((state) => state.plants.plants);
+
+  const navigate = useNavigate();
+  const handleProductDetails = (id) => {
+    navigate(`/products/${id}`);
+  };
 
   return (
     <>
       <h1>Listagem completa</h1>
-      <div className="product-list">
-      {plants
+      <div className={styles.productList}>
+        {plants
           .filter((plant) => (listagemTotal ? true : plant.isInSale === filter))
           .map((plant) => (
             <div key={plant.id}>
-              <CardContent
-                id={plant.id}
-                name={plant.name}
-                price={plant.price}
-                label={plant.label[1]}
-              />
+              <button onClick={() => handleProductDetails(plant.id)}>
+                <CardContent
+                  id={plant.id}
+                  name={plant.name}
+                  price={plant.price}
+                  label={plant.label[1]}
+                />
+              </button>
             </div>
           ))}
       </div>
