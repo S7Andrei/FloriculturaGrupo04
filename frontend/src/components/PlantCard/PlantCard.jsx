b1afa1e217426a5d1ec3dcf9b202764605380e4b
Pedro Souza <pedrosport10@gmail.com>
2023-12-28 23:19:40 -0300

    REFACTOR: Ajustando plantCard com outro componente

--- frontend/src/components/PlantCard/PlantCard.jsx
+++ frontend/src/components/PlantCard/PlantCard.jsx
@@ -1,55 +1,45 @@
 /* eslint-disable react/prop-types */
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { deletePlant } from "../../store/plants/plantsAction";
-import { NavLink } from "react-router-dom";
+import "./styles.css";
+
+import CardContent from "../CardContent/CardContent";
 
 const PlantCard = ({ listagemTotal = false, filter }) => {
   const plants = useSelector((state) => state.plants.plants);
-  const dispatch = useDispatch();
-
-  const handleDelete = (id) => {
-    dispatch(deletePlant(id));
-  };
 
   return (
     <>
-      {!listagemTotal
-        ? plants
+      {!listagemTotal ? (
+        <div className="product-list">
+          {plants
             .filter((plant) => plant.isInSale === filter)
             .map((plant) => {
               return (
-                <div key={plant.id} className="card">
-                  <div className="card-content">
-                    {plant.name}
-                    {plant.description}
-                    {plant.isInSale}
-                    <button
-                      type="button"
-                      onClick={() => handleDelete(plant.id)}
-                    >
-                      Deletar
-                    </button>
-
-                    <NavLink to={"/products"}>Products</NavLink>
-                  </div>
-                </div>
+                <CardContent
+                  key={plant.id}
+                  id={plant.id}
+                  name={plant.name}
+                  price={plant.price}
+                  label={plant.label[1]}
+                />
               );
-            })
-        : plants.map((plant) => {
+            })}
+        </div>
+      ) : (
+        <div className="product-list">
+          {plants.map((plant) => {
             return (
-              <div key={plant.id} className="card">
-                <div className="card-content">
-                  <li>{plant.name}</li>
-                  <button type="button" onClick={() => handleDelete(plant.id)}>
-                    Deletar
-                  </button>
-
-                  <NavLink to={"/products"}>Products</NavLink>
-                </div>
-              </div>
+              <CardContent
+                key={plant.id}
+                id={plant.id}
+                name={plant.name}
+                price={plant.price}
+                label={plant.label[1]}
+              />
             );
           })}
+        </div>
+      )}
     </>
   );
 };
