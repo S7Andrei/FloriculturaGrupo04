9962115f1557df8f39d61d2a3ef2b942868aea9a
Pedro Souza <pedrosport10@gmail.com>
2023-12-30 01:21:58 -0300

    FIX: Corringo planta background

--- frontend/src/pages/Home/HomeOne/HomeOne.jsx
+++ frontend/src/pages/Home/HomeOne/HomeOne.jsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router";
 import styles from "../Home.module.css";
 import ButtonHome from "../../../components/UI/Home/ButtonHome/ButtonHome";
-import arrow from '../../../assets/Arrow 1.svg'
+import arrow from "../../../assets/Arrow 1.svg";
+import plant from "../../../assets/plantForm.svg";
 
 const HomeOne = () => {
   const navigate = useNavigate();
@@ -13,22 +14,30 @@ const HomeOne = () => {
   return (
     <>
       <section className={styles.homeOneContainer}>
-        <h1 className={styles.h1}>
-          Discover your green <span id={styles.colorGreen}>side</span>
-        </h1>
+        <div className={styles.teste}>
+          <h1 className={styles.h1}>
+            Discover your green <span id={styles.colorGreen}>side</span>
+          </h1>
 
-        <p id={styles.bio}>
-          We are your one-stop destination for all things green and growing. Our
-          website offers a wide array of stunning plants, ranging from vibrant
-          flowers to lush indoor foliage, allowing you to create your very own
-          green oasis.
-        </p>
+          <p id={styles.bio}>
+            We are your one-stop destination for all things green and growing.
+            Our website offers a wide array of stunning plants, ranging from
+            vibrant flowers to lush indoor foliage, allowing you to create your
+            very own green oasis.
+          </p>
 
-        <ButtonHome onClick={handleProductDetails} id={styles.btn}>
-          Shop now
-        </ButtonHome>
+          <ButtonHome onClick={handleProductDetails} id={styles.btn}>
+            Shop now
+          </ButtonHome>
 
-        <p id={styles.learn}>Learn Gardening <img src={arrow} /></p>
+          <p id={styles.learn}>
+            Learn Gardening <img src={arrow} />
+          </p>
+        </div>
+
+        <figure>
+          <img src={plant} id={styles.img} />
+        </figure>
       </section>
     </>
   );
