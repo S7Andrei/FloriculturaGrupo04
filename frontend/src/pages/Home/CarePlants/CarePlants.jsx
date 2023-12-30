56b449a023c10a2882115bb7d0c86e6a0273cae7
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2023-12-30 15:08:48 -0300

    FEAT: Adicionado o conteudo estático do component care your plants

--- frontend/src/pages/Home/CarePlants/CarePlants.jsx
+++ frontend/src/pages/Home/CarePlants/CarePlants.jsx
@@ -2,15 +2,20 @@ import styles from "./CarePlants.module.css";
 import watering from "../../../assets/watering.svg";
 import sunligth from "../../../assets/sunligth.svg";
 import nutrientsAndFertilizing from "../../../assets/nutrientsAndFertilizing.svg";
+import ButtonHome from "../../../components/UI/Home/ButtonHome/ButtonHome";
+
+import carePlantsImg1 from "../../../assets/careYourPlantsFoto1.png";
+import carePlantsImg2 from "../../../assets/careYourPlantsFoto2.png";
+import carePlantsImg3 from "../../../assets/careYourPlantsFoto3.png";
 
 function CarePlants() {
   return (
-    <section>
+    <section className={styles.carePlantsContainer}>
       <div>
-        <h2 className={styles.carePlantcarPlantsSubtitlesTitle}>
+        <h2 className={styles.carePlantsTitle}>
           Stepes To Take Care Of Your Plants
         </h2>
-        <p className={styles.carPlantsSubtitle}>
+        <p className={styles.carePlantsSubtitle}>
           By following these three steps - proper watering, appropriate
           sunlight, and providing essential nutrients - you'll be well on your
           way to maintaining healthy and thriving plants.
@@ -61,6 +66,35 @@ function CarePlants() {
             </p>
           </li>
         </ul>
+      </div>
+
+      <div>
+        <figure>
+          <img src={carePlantsImg1} alt="" />
+        </figure>
+        <figure>
+          <img src={carePlantsImg2} alt="" />
+        </figure>
+        <div>
+          <figure>
+            <img src={carePlantsImg3} alt="" />
+          </figure>
+          <p className={styles.carPlantsSubtitle}>
+            Our website offers a wide array of stunning plants, ranging from
+            vibrant flowers to lush indoor foliage, allowing you to create your
+            very own green oasis. In addition to our extensive plant selection,
+            we also provide gardening kits and fertilizers to equip you with
+            everything you need to nurture your plants and achieve gardening
+            success. But we don't stop there! We believe that knowledge is the
+            key to a thriving garden, so we offer a wealth of information and
+            resources on gardening techniques, plant care tips, and landscaping
+            ideas. Whether you're a seasoned gardener or just starting your
+            green journey, our goal is to inspire and support you every step of
+            the way. Get ready to explore our virtual garden and discover the
+            joys of gardening with us!
+          </p>
+          <ButtonHome>See more photos</ButtonHome>
+        </div>
       </div>
     </section>
   );
