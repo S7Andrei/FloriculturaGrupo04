b8f93657e48b835d21d20a13ad19bdb86da0be8a
Rodhis <rodrigonoliva@gmail.com>
2023-12-30 16:49:54 -0300

    FEAT: início menu hamburguer

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import logo from "../../assets/logo.svg";
 import profile from "../../assets/profileLogo.svg";
+import { IoMenu } from "react-icons/io5";
+import { slide as Menu } from "react-burger-menu";
 
 import styles from "./Header.module.css";
 import Nav from "../UI/Nav/Nav";
@@ -29,6 +31,28 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+      <IoMenu className={styles.mobileNav}>
+        <Menu>
+          <nav>
+            <ul>
+              <li>
+                <Nav to={"/"} end>
+                  Home
+                </Nav>
+              </li>
+              <li>
+                <Nav to={"/register"}>Register</Nav>
+              </li>
+              <li>
+                <Nav to={"/products"}>Products</Nav>
+              </li>
+              <li>
+                <Nav to={"/about-us"}>About us</Nav>
+              </li>
+            </ul>
+          </nav>
+        </Menu>
+      </IoMenu>
       <figure>
         <img src={profile} alt="" style={{ width: "50px" }} />
       </figure>
