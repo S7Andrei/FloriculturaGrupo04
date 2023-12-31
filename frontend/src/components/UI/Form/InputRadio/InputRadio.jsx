3410de19496af5c6123e4111377422783df8c787
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2023-12-31 00:29:40 -0300

    FEAT: Adicionada a funcionalidade em que ao clicar no input radio o seu label muda de cor

--- frontend/src/components/UI/Form/InputRadio/InputRadio.jsx
+++ frontend/src/components/UI/Form/InputRadio/InputRadio.jsx
@@ -2,6 +2,14 @@ import { useEffect, useRef, useState } from "react";
 import styles from "./InputRadio.module.css";
 
 function InputRadio({ label, id, name, ...props }) {
+  const [isChecked, setIsChecked] = useState();
+  const input = useRef();
+
+  useEffect(() => {
+    const checked = input.current.checked;
+    setIsChecked(checked);
+  }, [input.current.checked]);
+
   return (
     <p className={styles.inputRadioContainer}>
       <input
@@ -9,9 +17,16 @@ function InputRadio({ label, id, name, ...props }) {
         name={name}
         id={id}
         className={styles.inputRadio}
+        checked={isChecked}
+        ref={input}
         {...props}
       />
-      <label htmlFor={id} className={styles.inputRadioLabel}>
+      <label
+        htmlFor={id}
+        className={`${styles.inputRadioLabel} ${
+          isChecked ? styles.checkedLabel : ""
+        }`}
+      >
         {label}
       </label>
     </p>
