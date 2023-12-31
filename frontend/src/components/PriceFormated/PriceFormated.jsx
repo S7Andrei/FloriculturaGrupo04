4c76b05c3c5c7a1728f82cd6c6ef899ad556f6a7
Pedro Souza <pedrosport10@gmail.com>
2023-12-31 02:23:17 -0300

    FEAT: Componente para formatação de moeda

---
+++ frontend/src/components/PriceFormated/PriceFormated.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+//import styles from './styles.module.css'
+
+const PriceFormated = ({price, discont, isInSale, styles}) => {
+
+    console.log(price, discont, isInSale)
+    const priceFormat = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(price);
+    
+      const numericPrice = parseFloat(price);
+      const numericDiscont = parseFloat(discont);
+      console.log(numericDiscont, numericPrice);
+    
+      const newPrice = numericPrice - (numericPrice * numericDiscont) / 100;
+    
+      const newPriceFormated = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(newPrice);
+
+  return (
+    <div>
+        {isInSale == 'promo' ? (
+          <div id={styles.priceContainer}>
+            <p id={styles.price}>{newPriceFormated}</p>
+            <p id={styles.oldPrice}>{priceFormat}</p>
+          </div>
+        ) : (
+          <p id={styles.price}>{priceFormat}</p>
+        )}
+    </div>
+  )
+}
+
+export default PriceFormated
