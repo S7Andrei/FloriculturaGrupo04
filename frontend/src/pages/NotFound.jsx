a1526a4c0d6735d7fb68bcae69e6c45c3631ceb4
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2023-12-30 22:14:57 -0300

    FEAT: Adicionado o header e o footer ao component NotFound

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,14 +1,17 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 const NotFound = () => {
   return (
     <>
+      <Header />
       <div>
         <h2>Sorry</h2>
         <p>That page cannot be found!</p>
         <Link to="..">Back to the homepage...</Link>
       </div>
+      <Footer />
     </>
   );
 };
