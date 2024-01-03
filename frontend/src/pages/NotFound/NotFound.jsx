15d24264c8c00a59652f5417e20eaf490e035fd9
Pedro Souza <pedrosport10@gmail.com>
2024-01-02 21:11:37 -0300

    FIX: Pequena correção de conflito de merge

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,46 +1,13 @@
-import { useNavigate } from "react-router-dom";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import { Link} from "react-router-dom";
 
 import styles from "./NotFound.module.css";
 import Cacto from "../../assets/404Cactus.png";
 import ButtonHome from "../../components/UI/Home/ButtonHome/ButtonHome";
 
 const NotFound = () => {
-  const navigate = useNavigate();
-
-  const handleHome = () => {
-    navigate(`/`);
-  };
-
   return (
     <>
-
-
       <div>
-<<<<<<< HEAD
-           
-        
-          <div data-custom="erContainer">
-          
-
-              <div>
-                <h1 data-custom="titleOps">
-                  Ops!
-                  <br />
-                  You Seem To Have Lost Your Way...
-                </h1>
-            <ButtonHome><Link to="/">Home</Link></ButtonHome>
-              </div>
-
-              <div data-custom="imgC">
-                <img src={Cacto} alt="e" />
-              </div>
-            
-            
-
-          </div>
-=======
         <div data-custom="erContainer">
           <div>
             <h1 data-custom="titleOps">
@@ -48,19 +15,16 @@ const NotFound = () => {
               <br />
               You Seem To Have Lost Your Way...
             </h1>
-            <div className={styles.btn}>
-              <ButtonHome onClick={handleHome}>Home</ButtonHome>
-            </div>
+            <ButtonHome>
+              <Link to="/">Home</Link>
+            </ButtonHome>
           </div>
 
-          <div>
+          <div data-custom="imgC">
             <img src={Cacto} alt="e" />
           </div>
         </div>
->>>>>>> 930ca72d4ca3a75832aa41b9776eb2b27ab3a2e1
       </div>
-
-     
     </>
   );
 };
