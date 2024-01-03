5427fe93e2eec82d283080bb271d92e3325c535f
Pedro Souza <pedrosport10@gmail.com>
2024-01-02 23:49:58 -0300

    FIX: Ajustando CSS fixo do split

--- frontend/src/components/SplitFeature/SplitFeature.jsx
+++ frontend/src/components/SplitFeature/SplitFeature.jsx
@@ -3,14 +3,12 @@ const SplitFeature = ({ plantSelected, styles }) => {
   const splitFeatures = plantSelected.features.split(/(?<=\.)\s*/);
 
   return (
-    <div className={styles.features}>
-      <div id={styles.featuresDetails}>
-        {splitFeatures.map((sentence, index) => (
-          <li key={index} id={styles.details}>
-            {sentence.trim()}
-          </li>
-        ))}
-      </div>
+    <div id={styles.featuresDetails}>
+      {splitFeatures.map((sentence, index) => (
+        <li key={index} id={styles.details}>
+          {sentence.trim()}
+        </li>
+      ))}
     </div>
   );
 };
