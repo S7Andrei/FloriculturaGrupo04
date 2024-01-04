e02ac98f166e3d3c5f022c9422bb817369207b8a
Andrei Silva <andreiluizsilva07@gmail.com>
2024-01-04 01:12:04 -0300

    REFACTOR: Ajustando o break do menu-burger.

---
+++ frontend/src/components/Header/mbBreak.js
@@ -0,0 +1,20 @@
+let hamburguerMenuStyles = {
+    bmOverlay: {
+     marginLeft: "-600px",
+      width: "100%",
+      height: "50px",
+      marginTop: "2.8em",
+      background: "rgba(0, 0, 0, 0.6)",
+    },
+    bmItem: {
+      fontFamily: "Raleway, cursive",
+      color: "#FFF",
+    },
+    bmItemList: {},
+    bmMenuWrap: {
+      width: "100%",
+      height: "40%",
+    },
+  };
+
+export default hamburguerMenuStyles;
