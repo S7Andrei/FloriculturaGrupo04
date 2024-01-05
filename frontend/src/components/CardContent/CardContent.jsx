595187a0afda89404ceb6aba60c1b1361023f8f0
Pedro Souza <pedrosport10@gmail.com>
2024-01-04 21:34:21 -0300

    FEAT: Ajuste no card, botão agora envolve a imagem e estilo do modal

--- frontend/src/components/CardContent/CardContent.jsx
+++ frontend/src/components/CardContent/CardContent.jsx
@@ -1,14 +1,36 @@
 /* eslint-disable react/prop-types */
-import styles from "./styles.module.css";
+//Componentes
 import PriceFormated from "../PriceFormated/PriceFormated";
 
+//CSS
+import styles from "./styles.module.css";
+
+//Libs
+import { useNavigate } from "react-router-dom";
+import { deletePlant } from "../../store/plants/plantsAction";
+import { useDispatch, useSelector } from "react-redux";
+
 const CardContent = ({ id, name, price, label, discont, isInSale, img }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const login = useSelector((state) => state.login.isLogado);
+
+  const handleProductDetails = (id) => {
+    navigate(`/products/${id}`);
+  };
+
+  const handleDelete = (id) => {
+    dispatch(deletePlant(id));
+  };
 
   return (
     <div key={id} className={styles.card}>
-      <div>
-        <img src={img} alt="Uma bela planta em seu vaso" />
-      </div>
+      <button onClick={() => handleProductDetails(id)}>
+        <div>
+          <img src={img} alt="Uma bela planta em seu vaso" />
+        </div>
+      </button>
+
       <div className={styles.cardContent}>
         <p id={styles.name}>{name}</p>
 
@@ -18,6 +40,7 @@ const CardContent = ({ id, name, price, label, discont, isInSale, img }) => {
           isInSale={isInSale}
           styles={styles}
         />
+        {login && <button onClick={() => handleDelete(id)}>Deletar</button>}
 
         <p id={styles.label}>{label}</p>
       </div>
